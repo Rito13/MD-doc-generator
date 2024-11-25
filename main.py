a2e96934cf481fb2872381b85c3d222aad7441fe
Rito12 <kapitan.cypek@gmail.com>
2024-11-25 21:57:44 +0100

    Tables
    Changed how xml is converted to markdown.
    Made generator of Properties table.
    Made generator of Methods table.

--- main.py
+++ main.py
@@ -4,72 +4,106 @@ import os
 import bbcode
 
 
-class XmlListConfig(list):
-    def __init__(self, aList):
-        for element in aList:
-            if element:
-                # treat like dict
-                if len(element) == 1 or element[0].tag != element[1].tag:
-                    self.append(XmlDictConfig(element))
-                # treat like list
-                elif element[0].tag == element[1].tag:
-                    self.append(XmlListConfig(element))
-            elif element.text:
-                text = element.text.strip()
-                if text:
-                    self.append(text)
+def table_of_members(members:ElementTree.Element) -> str:
+    table = "|Type|Name|Default Value|\n| - | - | - |\n"
+    for m in members:
+        try:
+            a = m.attrib
+            if not 'default' in a:
+                a['default'] = " "
+            table += '|'+a['type']+'|'+a['name']+'|'+a['default']+"|\n"
+        except Exception as error:
+            print("Error while interpretting members: ",error)
+            table += "| |Invalid member| |\n"
+    return table
 
+def decode_method(m:ElementTree.Element) -> tuple[str,str]:
+    return_type = ""
+    name = m.attrib["name"] + "("
+    params = {}
 
-class XmlDictConfig(dict):
-    def __init__(self, parent_element):
-        if parent_element.items():
-            self.update(dict(parent_element.items()))
-        for element in parent_element:
-            if element:
-                # treat like dict - we assume that if the first two tags
-                # in a series are different, then they are all different.
-                if len(element) == 1 or element[0].tag != element[1].tag:
-                    aDict = XmlDictConfig(element)
-                # treat like list - we assume that if the first two tags
-                # in a series are the same, then the rest are the same.
-                else:
-                    # here, we put the list in dictionary; the key is the
-                    # tag name the list elements all share in common, and
-                    # the value is the list itself
-                    aDict = {element[0].tag: XmlListConfig(element)}
-                # if the tag has attributes, add those to the dict
-                if element.items():
-                    aDict.update(dict(element.items()))
-                self.update({element.tag: aDict})
-            # this assumes that if you've got an attribute in a tag,
-            # you won't be having any text. This may or may not be a
-            # good idea -- time will tell. It works for the way we are
-            # currently doing XML configuration files...
-            elif element.items():
-                self.update({element.tag: dict(element.items())})
-            # finally, if there are no child tags and no attributes, extract
-            # the text
-            else:
-                self.update({element.tag: element.text})
+    for e in m:
+        try:
+            match e.tag:
+                case "return":
+                    return_type = e.attrib["type"]
+                case "param":
+                    params[e.attrib["index"]] = e.attrib
+        except:
+            continue
 
+    keys = list(params.keys())
+    keys.sort()
+    n = len(keys)
+    for i in range(n):
+        try:
+            if i != 0:
+                name += ", "
+            name += params[keys[i]]["type"] + " " + params[keys[i]]["name"]
+            try:
+                name += "=" + params[keys[i]]["default"]
+            except:
+                pass
+        except:
+            continue
+    name += ")"
 
-def dict_to_mark_down(D:dict,parser:bbcode.Parser) -> str:
+    return return_type,name
+
+def table_of_methods(methods:ElementTree.Element) -> str:
+    table = "|Return Type|Method|\n| - | - |\n"
+    for m in methods:
+        try:
+            type,method = decode_method(m)
+            table += '|'+type+'|'+method+'|'+"\n"
+        except Exception as error:
+            print("Error while interpretting members: ",error)
+            table += "| |Invalid member| |\n"
+    return table
+
+def xml_to_mark_down(root:ElementTree.Element,parser:bbcode.Parser) -> str:
+    D = {}
+    members = []
+    methods = []
+    constants = []
+
+    i = 0
+    while True:
+        try:
+            e = root[i]
+            match e.tag:
+                case "brief_description":
+                    D["brief_description"] = e.text
+                case "description":
+                    D["description"] = e.text
+                case "tutorials":
+                    D["tutorials"] = e.text
+                case "members":
+                    members = e
+                case "methods":
+                    methods = e
+        except:
+            break
+        i += 1
+
     D["brief_description"] = parser.format(D["brief_description"])
     D["description"] = parser.format(D["description"])
 
-    to_return = "# " + D["name"] + "\n\n**Inherits:** " + D["inherits"] + "\n" + D["brief_description"] + "\n"
+    to_return = "# " + root.attrib["name"] + "\n\n**Inherits:** " + root.attrib["inherits"] + "\n" + D["brief_description"] + "\n"
     to_return += "## Description \n" + D["description"] + "\n"
     to_return += "## Tutorials \n" + D["tutorials"] + "\n"
+    to_return += "## Properties \n" + table_of_members(members) + "\n"
+    to_return += "## Methods \n" + table_of_methods(methods) + "\n"
 
     return to_return
 
 # A custom programing language render function.
 def render_language(tag_name, value, options, parent, context):
-    return '**%s:**  \n```%s\n%s  \n```' % (tag_name, tag_name, value)
+    return '**%s:** \n```%s\n%s \n```' % (tag_name, tag_name, value)
 
 # A custom code render function.
 def render_code(tag_name, value, options, parent, context):
-    code_parser = bbcode.Parser(newline="  \n", escape_html=False, replace_links=False, replace_cosmetic=True)
+    code_parser = bbcode.Parser(newline=" \n", escape_html=False, replace_links=False, replace_cosmetic=True)
     code_parser.REPLACE_COSMETIC = (("    ", ""), ("\t", ""))
     for language in ["csharp","gdscript","python","cpp","text"]:
         code_parser.add_formatter(language, render_language, render_embedded=False, escape_html=False,replace_cosmetic=False)
@@ -78,7 +112,7 @@ def render_code(tag_name, value, options, parent, context):
 
 def create_bbcode_parser() -> bbcode.Parser:
     # Installing simple formatters.
-    parser = bbcode.Parser(newline="  \n",escape_html=False,replace_links=False,replace_cosmetic=True)
+    parser = bbcode.Parser(newline=" \n",escape_html=False,replace_links=False,replace_cosmetic=True)
     parser.REPLACE_COSMETIC = (("    ",""),("\t",""))
     parser.add_simple_formatter('b', '**%(value)s**')
     parser.add_simple_formatter('i', '*%(value)s*')
@@ -113,8 +147,7 @@ if __name__ == '__main__':
             continue
         tree = ElementTree.parse(xml_directory + file)
         root = tree.getroot()
-        xmldict = XmlDictConfig(root)
-        md_text = dict_to_mark_down(xmldict,parser)
+        md_text = xml_to_mark_down(root,parser)
 
         with open(md_directory + file[:-4] + '.md', 'w') as md_file:
             md_file.write(md_text[:-1])
